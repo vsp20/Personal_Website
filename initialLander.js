fcf596d8268e3bde04c05480f6bf925eca90f3f1
Vishal Patel <vsp20@case.edu>
2017-02-10 01:38:10 -0500

    Portfolio construction and navbar fully functional
    Worked on portfolio, but just starting to input apps, nav bar is fully
    functional

--- initialLander.js
+++ initialLander.js
@@ -12,12 +12,13 @@ $(document).ready(function(){
 		$rotate.fadeTo(1000,0.0)
 		setTimeout(function(){
 			$rotate.html(currentAdj)},1000);
-		setTimeout(function(){$rotate.fadeTo(1000,1)}	,2000);
+		setTimeout(function(){$rotate.fadeTo(1000,1)},2000);
 
 	}
 	setInterval(rotate,4000);
 	var $win = $(window);
 
+//Background image scrolling
     $('div.background').each(function(){
         var scroll_speed =5;
         var $this = $(this);
@@ -28,14 +29,26 @@ $(document).ready(function(){
         });
     });
 
+    $('div.portfolioPage').each(function(){
+        var scroll_speed =5;
+        var $this = $(this);
+        $(window).scroll(function() {
+            var bgScroll = -(($win.scrollTop() - $this.offset().top)/ scroll_speed);
+            var bgPosition = 'center '+ bgScroll + 'px';
+            $this.css({ backgroundPosition: bgPosition });
+        });
+    });
+
+
 
     // $('li').hover(function(){
     // 	$('li').css("background-color", '#f08080');
     
     // });
+    //nav bar color change while scrolling
     $(function(){
     	$(window).scroll(function(){
-	        var $scrollPercent = ($(document).scrollTop() / 1000);
+	        var $scrollPercent = ($(document).scrollTop() / 2000);
 
 	        if($scrollPercent <= 1){
 	            $('ul').css({backgroundColor:'rgba(101,76,97,'+$scrollPercent+')'});
@@ -50,16 +63,38 @@ $(document).ready(function(){
 //     }, 500);
 // });
 
+//tag movement
+// $('a[href^="#"]').on('click', function(event) {
+//     var target = $(this.getAttribute('href'));
+//     if( target.length ) {
+//         event.preventDefault();
+//         $('html, body').stop().animate({
+//             scrollTop: target.offset().top+250
+//         }, 1000);
+//     }
+// });
+$("a").on('click', function(event) {
 
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top+250
-        }, 1000);
-    }
-});
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top-60
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
 
 
 
